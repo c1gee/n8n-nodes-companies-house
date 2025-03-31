84f766f4ba694dc502c38a3c0dcfec95d1fd5e8a
Ian Carroll <iancarroll@Ians-Mac-mini.lan>
2025-03-31 09:16:46 +0100

    Fix authentication format and typing for Companies House credentials

--- nodes/CompaniesHouse/CompaniesHouse.node.ts
+++ nodes/CompaniesHouse/CompaniesHouse.node.ts
@@ -1,73 +1,93 @@
 import {
-	IExecuteFunctions,
-	INodeExecutionData,
-	INodeType,
-	INodeTypeDescription,
-	NodeConnectionType,
-	IHttpRequestOptions,
-	IHttpRequestMethods,
+  IExecuteFunctions,
+  INodeExecutionData,
+  INodeType,
+  INodeTypeDescription,
+  IHttpRequestOptions,
 } from 'n8n-workflow';
 
 export class CompaniesHouse implements INodeType {
-	description: INodeTypeDescription = {
-		displayName: 'Companies House',
-		name: 'companiesHouse',
-		group: ['transform'],
-		version: 1,
-		description: 'Search for UK company information via Companies House',
-		defaults: {
-			name: 'Companies House',
-		},
-		// eslint-disable-next-line n8n-nodes-base/node-class-description-inputs-wrong-regular-node
-		inputs: [NodeConnectionType.Main],
-		outputs: ['main'],
-		credentials: [
-			{
-				name: 'companiesHouseApi',
-				required: true,
-			},
-		],
-		properties: [
-			{
-				displayName: 'Company Name',
-				name: 'companyName',
-				type: 'string',
-				default: '',
-				placeholder: 'OpenAI Ltd',
-				required: true,
-				description: 'The name of the company to search for',
-			},
-		],
-	};
+  description: INodeTypeDescription = {
+    displayName: 'Companies House',
+    name: 'companiesHouse',
+    group: ['transform'],
+    version: 1,
+    description: 'Interact with the UK Companies House API',
+    defaults: {
+      name: 'Companies House',
+    },
+    inputs: ['main'],
+    outputs: ['main'],
+    credentials: [
+      {
+        name: 'companiesHouseApi',
+        required: true,
+      },
+    ],
+    properties: [
+      {
+        displayName: 'Operation',
+        name: 'operation',
+        type: 'options',
+        options: [
+          { name: 'Search Companies', value: 'search' },
+          { name: 'Get Company Profile', value: 'getProfile' },
+          { name: 'Get Officers', value: 'getOfficers' },
+        ],
+        default: 'search',
+        description: 'The operation to perform',
+      },
+      {
+        displayName: 'Company Name or Number',
+        name: 'companyInput',
+        type: 'string',
+        default: '',
+        required: true,
+        description: 'Company name (for search) or number (for profile/officers)',
+      },
+    ],
+  };
 
-	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		const items = this.getInputData();
-		const returnData: INodeExecutionData[] = [];
+  async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+    const items = this.getInputData();
+    const returnData: INodeExecutionData[] = [];
 
-		for (let i = 0; i < items.length; i++) {
-			const companyName = this.getNodeParameter('companyName', i) as string;
+    for (let i = 0; i < items.length; i++) {
+      const operation = this.getNodeParameter('operation', i) as string;
+      const companyInput = this.getNodeParameter('companyInput', i) as string;
 
-			const options: IHttpRequestOptions = {
-				method: 'GET' as IHttpRequestMethods,
-				url: `https://api.company-information.service.gov.uk/search/companies?q=${encodeURIComponent(companyName)}`,
-				json: true,
-			};
+      let endpoint = '';
 
-			const credentials = await this.getCredentials('companiesHouseApi');
-const authHeader = 'Basic ' + Buffer.from(credentials.user + ':').toString('base64');
+      switch (operation) {
+        case 'search':
+          endpoint = `/search/companies?q=${encodeURIComponent(companyInput)}`;
+          break;
+        case 'getProfile':
+          endpoint = `/company/${encodeURIComponent(companyInput)}`;
+          break;
+        case 'getOfficers':
+          endpoint = `/company/${encodeURIComponent(companyInput)}/officers`;
+          break;
+        default:
+          throw new Error(`Unsupported operation: ${operation}`);
+      }
 
-options.headers = {
-	Authorization: authHeader,
-};
+      const credentials = await this.getCredentials('companiesHouseApi');
+      const authHeader = 'Basic ' + Buffer.from(credentials.user + ':').toString('base64');
 
-const responseData = await this.helpers.httpRequest.call(this, options);
+      const options: IHttpRequestOptions = {
+        method: 'GET',
+        url: `https://api.company-information.service.gov.uk${endpoint}`,
+        headers: {
+          Authorization: authHeader,
+        },
+        json: true,
+      };
 
+      const response = await this.helpers.httpRequest.call(this, options);
+      returnData.push({ json: response });
+    }
 
-			returnData.push({
-				json: responseData,
-			});
-		}
-
-		return [returnData];
-	}
+    return [returnData];
+  }
 }
